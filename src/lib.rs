cfa8a770ab501d8b58ec138c22e977631326331c
ashtneoi <ashtneoi@gmail.com>
2019-03-10 21:27:23 -0700

    Remove unnecessary things

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-#[cfg(feature = "serde")]
-use serde::{Serialize, Deserialize};
 use std::fmt;
 use std::mem;
 
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct AbbrevTree<T> {
     v: Vec<(String, AbbrevTree<T>)>,
     pub data: T,
